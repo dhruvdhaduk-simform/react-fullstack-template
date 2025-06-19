2d06a54b6a0ebdcf762bb785e43f035585df6bd5
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-19 12:12:26 +0530

    Improve the UI for greeting in home page

--- apps/frontend/src/pages/HomePage.tsx
+++ apps/frontend/src/pages/HomePage.tsx
@@ -1,16 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { trpc } from '@/utils/trpc';
+import { Label } from '@radix-ui/react-label';
+import { useState } from 'react';
 
 export function HomePage() {
-    const greetingQuery = trpc.greeting.useQuery('Dhruv');
+    const [name, setName] = useState('');
+    const greetingQuery = trpc.greeting.useQuery(name, {
+        enabled: false,
+    });
 
     return (
-        <div>
-            <div>
-                {greetingQuery.isLoading && <p>Loading . . .</p>}
-                {greetingQuery.error && (
-                    <p className="text-red-500">Error: {greetingQuery.error.message}</p>
-                )}
-                {greetingQuery.data && <p>{greetingQuery.data.text}</p>}
+        <div className="pt-14">
+            {greetingQuery.isLoading && <p className="text-center">Loading . . .</p>}
+            {greetingQuery.error && (
+                <p className="text-center text-red-500">Error: {greetingQuery.error.message}</p>
+            )}
+            {greetingQuery.data && (
+                <p className="text-center font-bold text-3xl">{greetingQuery.data.text}</p>
+            )}
+
+            <div className="max-w-80 flex flex-col gap-4 p-4 mx-auto">
+                <Label>
+                    <span className="text-sm">Your Name</span>
+                    <Input value={name} onChange={(e) => setName(e.currentTarget.value)} />
+                </Label>
+                <Button onClick={() => greetingQuery.refetch()}>Submit</Button>
             </div>
         </div>
     );
