ce9237ccbf2993660807f8aad330f354ea3e910e
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-06-19 10:57:37 +0530

    Create the backend with tRPC

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,49 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+
+const t = initTRPC.create();
+
+const publicProcedure = t.procedure;
+const router = t.router;
+
+export const appRouter = router({
+    greeting: publicProcedure.input(z.string().nullish()).query(({ input }) => {
+        return {
+            text: `hello ${input ?? 'world'}`,
+        };
+    }),
+
+    reverse: publicProcedure.input(z.string()).query(({ input }) => {
+        return {
+            input,
+            result: `${input.split('').reverse().join('')}`,
+        };
+    }),
+
+    math: router({
+        square: publicProcedure.input(z.number()).query(({ input }) => {
+            return {
+                result: `Square of ${input} is ${input * input}`,
+            };
+        }),
+
+        sqrt: publicProcedure.input(z.number()).query(({ input }) => {
+            return {
+                result: `Square root of ${input} is ${Math.sqrt(input)}`,
+            };
+        }),
+
+        add: publicProcedure
+            .input(
+                z.object({
+                    a: z.number(),
+                    b: z.number(),
+                })
+            )
+            .query(({ input }) => {
+                return {
+                    result: `${input.a} + ${input.b} = ${input.a + input.b}`,
+                };
+            }),
+    }),
+});
---
+++ apps/backend/src/shared/trpc.ts
@@ -0,0 +1,3 @@
+import { appRouter } from '../index';
+
+export type AppRouter = typeof appRouter;
